d9cdf893f61cb14b5e32c1117839323d6a54e3b1
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 14:56:21 -0800

    adding click scene loader

---
+++ Assets/FractalArt/ClickHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickHandler : MonoBehaviour
+{
+	public string scene;
+
+	void OnMouseDown() {
+		Application.LoadLevel(scene);
+	}
+}
