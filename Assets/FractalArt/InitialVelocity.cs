d0a6bf360d2f247c287931c4fd908c57787572b1
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 12:22:34 -0800

    adding initial velocity instead of gravity. Also fixing right wall

---
+++ Assets/FractalArt/InitialVelocity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialVelocity : MonoBehaviour
+{
+	public Vector2 initialForce;
+	public Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddForce(initialForce);
+	}
+}
