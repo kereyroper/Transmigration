dcaa4da9227dfb292b569c816a45c05878def98e
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 14:13:59 -0800

    memory manager stub

---
+++ Assets/Game/MemoryManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryManager : MonoBehaviour
+{
+	private PlayerState playerState;
+
+	// Use this for initialization
+	void Start () {
+		var globals = GameObject.Find("Globals");
+		playerState = globals.GetComponent<PlayerState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
