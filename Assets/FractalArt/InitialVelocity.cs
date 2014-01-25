3791c1303484c7cf1c332c09d0b32053435c3a70
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 14:31:24 -0800

    fixing member hiding

--- Assets/FractalArt/InitialVelocity.cs
+++ Assets/FractalArt/InitialVelocity.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class InitialVelocity : MonoBehaviour
 {
 	public Vector2 initialForce;
-	public Rigidbody2D rigidbody2D;
 
 	// Use this for initialization
 	void Start () {
