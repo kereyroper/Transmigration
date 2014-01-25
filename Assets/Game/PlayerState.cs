c56aee12ab48d847f30baf027a3dbda28152ba96
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 13:40:56 -0800

    adding PlayerState

---
+++ Assets/Game/PlayerState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour
+{
+	public float happiness; // sadness
+	public float courage; // fear
+	public float love; // hate
+	public float friendship; // enemyship
+	public float calm; // anger
+}
