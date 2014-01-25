ae5217bb9f841b0b789154560b8fb79c565f7278
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 15:07:11 -0800

    programatically setting inital background for TV

---
+++ Assets/Rooms/TV/InitialBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var spritePrefab = Resources.Load<Sprite>("tvStart01");
+		var sprite = (Sprite)Instantiate(spritePrefab);
+		var spriteRender = gameObject.GetComponent<SpriteRenderer>();
+		spriteRender.sprite = sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
