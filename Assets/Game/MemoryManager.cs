4cbed2fcf1e09dfdc26f349deabf85f2a5a61d1c
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 14:32:12 -0800

    initialize fractal from actual art

--- Assets/Game/MemoryManager.cs
+++ Assets/Game/MemoryManager.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class MemoryManager : MonoBehaviour
 {
+	public GameObject[] spriteGos;
+	public GameObject spriteGoPrefab;
 	private PlayerState playerState;
 
 	// Use this for initialization
 	void Start () {
-		var globals = GameObject.Find("Globals");
-		playerState = globals.GetComponent<PlayerState>();
+//		var globals = GameObject.Find("Globals");
+//		playerState = globals.GetComponent<PlayerState>();
+		var spritePrefab = Resources.Load<Sprite>("tv01");
+		var sprite = (Sprite)Instantiate(spritePrefab);
+		spriteGos = new GameObject[10];
+		var spriteGo = (GameObject)Instantiate(spriteGoPrefab);
+		spriteGo.GetComponent<SpriteRenderer>().sprite = sprite;
+		spriteGos[0] = spriteGo;
 	}
 	
 	// Update is called once per frame
