3e6bb8a656e454cc6e0eaa97b1ac88a17b3202b1
Kerey Roper <kerey@rivetinggames.com>
2014-01-24 21:32:54 -0800

    adding simple timer and trigger

---
+++ Assets/Game/MakeGlobal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeGlobal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+	}
+	
+}
