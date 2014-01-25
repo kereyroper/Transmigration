3d97694cee9d3f24055fd7edfa833086c218534f
Kerey Roper <kerey@rivetinggames.com>
2014-01-24 21:55:42 -0800

    adding some initialization framework

---
+++ Assets/Game/Initialize.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initialize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (!GameObject.Find("Globals")) {
+			Application.LoadLevel("InitializeGlobals");
+		} else {
+			var dayGlobals = GameObject.Find("DayGlobals");
+			if (dayGlobals != null) {
+				Destroy(dayGlobals);
+			}
+			Application.LoadLevel("InitializeDay");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Game/InitializeDay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeDay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad(GameObject.Find("DayGlobals"));
+		Application.LoadLevel("Living");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
