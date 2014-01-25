49e789649a451c6b4e58ca0f3f8d75b96fc78ccf
0cean2 <hainh96@gmail.com>
2014-01-24 21:21:24 -0800

    Some Dialog editor class
    Will try working on it

--- Assets/Game/Dialog.cs
+++ Assets/Game/Dialog.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class Dialog : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Game/DialogBoxEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogBoxEditor : MonoBehaviour {
+
+	public int width, height, content;
+	private void updateSize();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if width, height or content changed
+		//updateSize()
+	}
+
+	void updateSize(){
+
+	}
+}
