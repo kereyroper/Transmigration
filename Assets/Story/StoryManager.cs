59bf7001d031d0421be7428cd74cfe83e205bc26
Kerey Roper <kerey@rivetinggames.com>
2014-01-25 16:58:26 -0800

    adding stub json

---
+++ Assets/Story/StoryManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class StoryManager : MonoBehaviour
+{
+	public TextAsset storyJsonFile;
+
+	// Use this for initialization
+	void Start () {
+		var storyJson = JSONNode.Parse(storyJsonFile.text);
+		Debug.Log(storyJson["storyElements"]["prompt"]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
